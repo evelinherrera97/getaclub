a4515d06cb60cc7fe8bc78f61570b6f2538e6cd3
evelin.herrera <evelin.herrera@banistmo.com>
2021-09-30 23:13:03 -0500

    loading

--- src/app/serivces/main.service.ts
+++ src/app/serivces/main.service.ts
@@ -1,38 +1,34 @@
 import { Injectable } from '@angular/core';
 import { AngularFirestore } from '@angular/fire/firestore';
 import { AngularFireStorage } from '@angular/fire/storage';
-import { AngularFireDatabase } from '@angular/fire/database';
 import { Observable } from 'rxjs';
 import { finalize } from 'rxjs/operators';
+import { FileUpload } from '../shared/interface/file-upload';
+import { Form } from '../shared/interface/form';
 
 @Injectable({
   providedIn: 'root'
 })
 export class MainService {
-
-
   public character: Observable<any>;
 
   constructor(
-    private db: AngularFireDatabase,
     public afs: AngularFirestore,
     private storage: AngularFireStorage
   ) { }
 
 
   public getCharacters(): Observable<any> {
-    this.character = this.afs.collection<any>('character').valueChanges();
-
+    this.character = this.afs.collection<Form>('character').valueChanges();
     return this.character;
   }
 
-
-  public createAccount(data: any, downloadURL:any) {
-    data.value.avatar = downloadURL;
-    this.afs.collection('character').add(data.value);
+  public createAccount(data: Form, downloadURL: string) {
+    data.avatar = downloadURL;
+    this.afs.collection('character').add(data);
   }
 
-  public uploadImage(dataForm:any, image:FileUpload) : Observable<any> {
+  public uploadImage(dataForm: Form, image: FileUpload): Observable<any> {
     const file = `image/${image.name}`
     const fileRef = this.storage.ref(file)
     const uploadTask = this.storage.upload(file, image);
@@ -47,19 +43,7 @@ export class MainService {
     return uploadTask.percentageChanges();
   }
 
-
-
-  // private deleteFileDatabase(key: string): Promise<void> {
-  //   return this.afs.collection('character').remove(key);
-  // }
-
-  
 }
 
-export class FileUpload {
-  key: string;
-  name: string;
-  url: string;
-  file: File;
-} 
+
 
---
+++ src/app/shared/interface/form.ts
@@ -0,0 +1,11 @@
+export interface Form {
+    nameCharacter: string,
+    ageCharacter: number,
+    rolCharacter: string,
+    personality: string,
+    descriptionRol: string,
+    ability: string,
+    date: Date,
+    creatorName: string,
+    avatar: string,
+}
